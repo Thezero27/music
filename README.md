3d4169703a64c553d93b770b1144a0a608912912
Thezero <67761713+Thezero27@users.noreply.github.com>
2020-12-19 00:16:45 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lyrics-Music
+
+App Buscador de letras de canciones al gusto!🎶🎶
+
+## Lyrics-Music(Info)
+
+-Responsive Design <br>
+-Idioma de letras ESPAÑOL <br>
+-Canciones Mundiales <br>
+-Api Consumida : https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search
+
+
+[Demostracion Web](https://crismaxis.github.io/Lyrics-Music/?#)
+
+![Imagen Paso 1](https://raw.githubusercontent.com/Crismaxis/AssetsInfo/master/asset/LyricSong.png)
---
+++ js/main.js
@@ -0,0 +1,48 @@
+/* VALIDAR Y CAPTURAR VALORES INPUT */
+const Buscador = document.querySelector('#Buscar');
+Buscador.addEventListener('click', e => {
+    e.preventDefault();
+    const Artista = document.querySelector('#Artista').value;
+    const Cancion = document.querySelector('#Cancion').value;
+    if (Artista === '' || Cancion === '') {
+        document.querySelector('.Error').innerText = 'Campos Vacios';
+        setTimeout(function () {
+            document.querySelector('.Error').innerText = '';
+        }, 1200);
+
+    } else {
+        SearchSong(Artista, Cancion);
+        document.querySelector('#Title').innerText = `${Cancion}`;
+        /* VACIAR FORM */
+        document.querySelector('#Artista').value = '';
+        document.querySelector('#Cancion').value = '';
+    }
+});
+
+/* CERRAR MODAL X */
+const CerrarModal = document.querySelector('#Cerrar');
+CerrarModal.addEventListener('click', () => {
+    document.querySelector('.modal').style.display = 'none';
+});
+
+/* CONSUMIR API */
+function SearchSong(Artista, Cancion) {
+    let Url = `https://api.lyrics.ovh/v1/${Artista}/${Cancion}`;
+
+    fetch(Url)
+        .then(res => {
+            return res.json();
+        })
+        .then(datos => {
+            if (datos.lyrics === '') {
+                alert('Not found song');
+            } else {
+                document.querySelector('#Song').innerText = `${datos.lyrics}`;
+                document.querySelector('.modal').style.display = 'flex';
+            }
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
